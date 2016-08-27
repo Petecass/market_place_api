4e6da501a4260a77bedd6407ed4b8660f03c0062
petecass <pete@petecss.com>
2016-08-28 00:20:56 +0800

    adds first user specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    password 'vivalavida'
+    password_confirmation 'vivalavida'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = build(:user) }
+
+  subject { @user }
+
+  it 'is a valid user' do
+    is_expected.to respond_to(:email)
+    is_expected.to respond_to(:password)
+    is_expected.to respond_to(:password_confirmation)
+    is_expected.to be_valid
+  end
 end
